c618e9258796641770c6b3c3b122ec57032880ce
eddieduro <e.jamesduro@gmail.com>
2016-02-19 09:38:04 -0800

    add form to index

--- app/app.php
+++ app/app.php
@@ -1,11 +1,14 @@
 <?php
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../vendor/RepeatCounter.php';
+    require_once __DIR__.'/../src/RepeatCounter.php';
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider, array(
         "twig.path" => __DIR__.'/../views'
     ));
 
+    $app->get('/', function () use ($app){
+        return $app['twig']->render('index.html.twig');
+    });
     return $app;
 ?>

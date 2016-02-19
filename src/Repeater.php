7a00644c4b2f7713d42d43e0d58154fcbe97f665
eddieduro <e.jamesduro@gmail.com>
2016-02-19 09:00:10 -0800

    add first passing spec

---
+++ src/Repeater.php
@@ -0,0 +1,10 @@
+<?php
+    class Repeater
+    {
+        function countRepeats($sentence)
+        {
+            $lowerCase_sentence =  strtolower($sentence);
+            return $lowerCase_sentence;
+        }
+    }
+?>
---
+++ tests/RepeaterTest.php
@@ -0,0 +1,31 @@
+<?php
+    require 'src/Repeater.php';
+
+    class RepeaterTest extends PHPUnit_Framework_TestCase
+    {
+        function test_stringToLower()
+        {
+            // Arrange
+            $new_Repeater = new Repeater;
+            $input = "wooHoO";
+
+            // Act
+            $result = $new_Repeater->countRepeats($input);
+
+            // Assert
+            $this->assertEquals("woohoo", $result);
+        }
+        // function test_sentenceSplit()
+        // {
+        //     // Arrange
+        //     $new_Repeater = new Repeater;
+        //     $input = "hey you";
+        //
+        //     // Act
+        //     $result = $new_Repeater->countRepeats($input);
+        //
+        //     // Assert
+        //     $this->assertEquals(array('hey','you'), $result);
+        // }
+    }
+?>

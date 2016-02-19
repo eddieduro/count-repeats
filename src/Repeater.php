5848673ac380f3c5c21908e5ce17bb2925bf86de
eddieduro <e.jamesduro@gmail.com>
2016-02-19 09:09:49 -0800

    add 3rd passing spec and update previous tests to pass new condition

--- src/Repeater.php
+++ src/Repeater.php
@@ -1,12 +1,20 @@
 <?php
     class Repeater
     {
-        function countRepeats($sentence)
+        function countRepeats($sentence, $word_search)
         {
+            $appears = 0;
             $lowerCase_sentence =  strtolower($sentence);
             $word_array = explode(" ", $lowerCase_sentence);
 
-            return $word_array;
+            foreach($word_array as $word)
+            {
+                if(count_chars($word) == count_chars($word_search))
+                {
+                    $appears = $appears + 1;
+                }
+            }
+            return $appears;
         }
     }
 ?>
--- tests/RepeaterTest.php
+++ tests/RepeaterTest.php
@@ -8,24 +8,38 @@
             // Arrange
             $new_Repeater = new Repeater;
             $input = "wooHoO";
-
+            $input2 = "yo";
             // Act
-            $result = $new_Repeater->countRepeats($input);
+            $result = $new_Repeater->countRepeats($input, $input2);
 
             // Assert
-            $this->assertEquals(array("woohoo"), $result);
+            $this->assertEquals(0, $result);
         }
         function test_sentenceSplit()
         {
             // Arrange
             $new_Repeater = new Repeater;
             $input = "hey you";
+            $input2 = "yo";
 
             // Act
-            $result = $new_Repeater->countRepeats($input);
+            $result = $new_Repeater->countRepeats($input, $input2);
 
             // Assert
-            $this->assertEquals(array('hey','you'), $result);
+            $this->assertEquals(0, $result);
+        }
+
+        function test_searchMatchingWords()
+        {
+            // Arrange
+            $new_Repeater = new Repeater;
+            $input = "hey you hey hey";
+            $input2 = "hey";
+            // Act
+            $result = $new_Repeater->countRepeats($input, $input2);
+
+            // Assert
+            $this->assertEquals(3, $result);
         }
     }
 ?>
